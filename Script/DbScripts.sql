bf289076fe89725f57b65938f37ba84b8468ac95
gorakkhule07 <52404610+gorakkhule07@users.noreply.github.com>
2020-07-22 14:56:52 +0530

    Changes done for Mysql to MS-SQL

--- Script/DbScripts.sql
+++ Script/DbScripts.sql
@@ -1,102 +1,110 @@
-CREATE TABLE `contact` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(45) DEFAULT NULL,
-  `LastName` varchar(45) DEFAULT NULL,
-  `PhoneNumber` varchar(45) DEFAULT NULL,
-  `Status` varchar(45) DEFAULT NULL,
-  `Email` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`Id`)
+CREATE TABLE [dbo].[contact] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]   VARCHAR (45) NULL,
+    [LastName]    VARCHAR (45) NULL,
+    [PhoneNumber] VARCHAR(50)   NULL,
+    [Status]      VARCHAR (45) NULL,
+    [Email]       VARCHAR(45)   NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
+GO
 
-DELIMITER $$
-CREATE  PROCEDURE `SP_Contacts_D_ById_contact`(
-IN iId int
-)
+CREATE PROCEDURE [dbo].SP_Contacts_D_ById_contact(
+	@iId int 
+	 )
+AS
 BEGIN
-update contact set Status="InActive" where Id= iId;
-END$$
-DELIMITER ;
+	update contact set Status = 'InActive' where Id= @iId;
+END
 
-DELIMITER $$
-CREATE  PROCEDURE `SP_Contacts_I_contact`(
-IN iEmail Varchar(45),
-IN iFirstName Varchar(45),
-IN iLastName Varchar(45),
-IN iPhoneNumber Varchar(45),
-IN iStatus Varchar(45)
-)
+GO
+
+CREATE PROCEDURE [dbo].SP_Contacts_G_ALL_contact
+AS
 BEGIN
+	select * from contact;
+END
 
-insert into contact(
-FirstName,
-LastName,
-PhoneNumber,
-Status,
-Email
-)
-values(
-iFirstName,
-iLastName,
-iPhoneNumber,
-iStatus,
-iEmail
-);
+GO
 
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE  PROCEDURE `SP_Contacts_G_ById_contact`(
-IN iId int
-)
+CREATE PROCEDURE [dbo].SP_Contacts_G_ById_contact
+	@iId int
+AS
 BEGIN
-select * from contact where Id = iId;
-END$$
-DELIMITER ;
+	select * from contact where Id = @iId;
+END
 
-DELIMITER $$
-CREATE PROCEDURE `SP_Contacts_G_ALL_contact`()
-BEGIN
-select * from contact;
-END$$
-DELIMITER ;
+GO
 
-DELIMITER $$
-CREATE PROCEDURE `SP_Contacts_G_Email_count`(
-IN iId int,
-IN iEmail varchar(45)
-)
+CREATE PROCEDURE [dbo].SP_Contacts_G_Email_count
+(
+ @iId int,
+ @iEmail varchar(45)
+ )
+AS
 BEGIN
 
-if iId = 0
-then
-	select count(*) from contact where lower(Email)= lower(iEmail);
-else
-   select count(*) from contact where lower(Email)= lower(iEmail) and Id != iId;
-end if;
-END$$
-DELIMITER ;
+	IF @iId = 0
+		BEGIN
+			select count(*) from contact where LOWER(Email) = LOWER(@iEmail);
+		END
+	ELSE
+		BEGIN
+		   select count(*) from contact where LOWER(Email)= LOWER(@iEmail) and Id <> @iId;
+		end;
+	
 
-DELIMITER $$
-CREATE PROCEDURE `SP_Contacts_U_contact`(
-IN iId int,
-IN iEmail Varchar(45),
-IN iFirstName Varchar(45),
-IN iLastName Varchar(45),
-IN iPhoneNumber Varchar(45),
-IN iStatus Varchar(45)
+END
+
+
+GO
+CREATE PROCEDURE [dbo].SP_Contacts_I_contact(
+@iEmail Varchar(45),
+@iFirstName Varchar(45),
+@iLastName Varchar(45),
+@iPhoneNumber Varchar(45),
+@iStatus Varchar(45)
 )
+	
+AS
+	BEGIN
+		insert into contact(
+		FirstName,
+		LastName,
+		PhoneNumber,
+		Status,
+		Email
+		)
+		values(
+		@iFirstName,
+		@iLastName,
+		@iPhoneNumber,
+		@iStatus,
+		@iEmail
+		);
+
+	END
+	
+GO
+
+CREATE PROCEDURE [dbo].SP_Contacts_U_contact
+(
+@iId int,
+@iEmail Varchar(45),
+@iFirstName Varchar(45),
+@iLastName Varchar(45),
+@iPhoneNumber Varchar(45),
+@iStatus Varchar(45)
+)
+AS
 BEGIN
-UPDATE  contact SET
-FirstName=iFirstName,
-LastName=iLastName,
-PhoneNumber=iPhoneNumber,
-Status=iStatus,
-Email=iEmail where Id=iId;
+UPDATE  
+contact SET
+FirstName=@iFirstName,
+LastName=@iLastName,
+PhoneNumber=@iPhoneNumber,
+Status=@iStatus,
+Email=@iEmail where Id=@iId;
 
-END$$
-DELIMITER ;
-
-
-
+END
